a151b5211057b35ced2cc676b91c81b9534163b2
Valeriy <mypowerismind@gmail.com>
2015-02-26 00:22:20 +0200

    Accounts avaliable

--- engine.js
+++ engine.js
@@ -36,7 +36,8 @@ $(function() {
 	var UserTasksView = Parse.View.extend({
 		
 		events: {
-			"click #task-add" : "createTaskView"
+			"click #task-add" : "createTaskView",
+			"click .user-logout" : "logout"
 		},
 
 		el: '.content',
@@ -66,26 +67,104 @@ $(function() {
 
 		addAll: function(){
 			this.$('#task-list').html("");
-			
-			var self = this;
+		
+			this.tasks.each(this.addTask);
+		},
 
-			this.tasks.each(function(task){
-				self.addTask(task);
-			});
+		logout: function(){
+			Parse.User.logOut();
+			new LogInView();
+			delete this;
 		},
 
 		createTaskView: function(e){
 			var self = this;
-			this.tasks.create({
-				content: self.input.val()
+			console.log(Parse.User.current());
+			var newTask = new Task({
+				content: self.input.val(),
+				ACL: new Parse.ACL(Parse.User.current())
 			});
-
-			self.input.val('');
-			self.addAll();
+			newTask.save().then(function(){
+				self.tasks.add(newTask);
+				self.input.val('');
+				self.addAll();	
+			});		
 		}
 	});
 
-	//new UserTasksView();
+	var LogInView = Parse.View.extend({
+		events: {
+			"submit form.login-form" : "logIn",
+			"submit form.signup-form" : "signUp"
+		},
+
+		el: ".content",
+
+		initialize: function(){
+			this.render();
+		},
+
+		logIn: function(e){
+			var self = this;
+			var username = this.$('#login-username').val();
+			var password = this.$('#login-password').val();
+
+			Parse.User.logIn(username, password, {
+				success: function(user){
+					new UserTasksView();
+					delete self;
+				},
+
+				error: function(user, error){
+					self.$(".login-form .error").html("Invalid username or password. Please try again!").show();
+					self.$(".login-form button").removeAttr("disabled");
+				}
+			});
+
+			this.$(".login-form button").attr("disabled", "disabled");
+
+			return false;
+		},
+
+		signUp: function(e){
+			var self = this;
+			var username = this.$("#signup-username").val();
+			var password = this.$("#signup-password").val();
+
+			Parse.User.signUp(username, password, { ACL: new Parse.ACL()},{
+				success: function(user){
+					new UserTasksView();
+					delete self;
+				},
+				error: function(user, error){
+					self.$(".signup-form .error").html(error.message).show();
+					self.$("signup-form button").removeAttr("disabled");
+				}
+			});
+
+			this.$(".signup-form button").attr("disabled","disabled");
+
+			return false;
+		},
+
+		render: function(){
+			this.$el.html(Handlebars.compile($('#user-login-template').html()));
+		}
+	});
+
+	var AppView = Parse.View.extend({
+		el: $("#futureCheckListApp"),
+
+		initialize: function(){
+			if (Parse.User.current()){
+				new UserTasksView();
+			} else {
+				new LogInView();
+			}
+		}
+	});
+	
+	new AppView();
 
 });
 
